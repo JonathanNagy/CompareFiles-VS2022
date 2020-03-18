d202119dd5362e07179b458a592beaff6b5d8c25
JonathanNagy <jonathan.a.nagy@gmail.com>
2020-03-18 08:17:45 -0400

    Adding VS2017 project back in

---
+++ CompareFiles-VS2017/CompareFilesMultiCommand.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
+using CompareFiles.Common;
+using EnvDTE;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Task = System.Threading.Tasks.Task;
+
+namespace CompareFilesVS2017
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class CompareFilesMultiCommand : CompareFilesCommandBase
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 4131;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("338b3aa8-bf08-4e90-8668-c2d5f3164b9c");
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CompareFilesCommand"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        /// <param name="appObject">DTE event object</param>
+        private CompareFilesMultiCommand(AsyncPackage package, OleMenuCommandService commandService, DTE appObject) : base(package, appObject)
+        {
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.Execute, menuCommandID);
+            commandService.AddCommand(menuItem);
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static CompareFilesMultiCommand Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            // Switch to the main thread - the call to AddCommand in CompareFilesCommand's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            var commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+            var appObject = await package.GetServiceAsync(typeof(DTE)) as DTE;
+            Instance = new CompareFilesMultiCommand(package, commandService, appObject);
+        }
+
+    }
+}
